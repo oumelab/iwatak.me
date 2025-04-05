bc95459b695bf6852573a0cb162e8901ed77c5da
Keiko Iwata <oumelab@pt.rdy.jp>
2025-04-05 18:55:26 +0900

    fix: 記事を修正

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className="max-w-[580px] mx-auto py-2">
       <p className="">ポートフォリオとWeb開発の学習雑記帳。<br />フロントエンド中心に学んだことをメモしています。</p>
-      <p>現在求職中。</p>
     </div>
   );
 }
--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-// import Link from "next/link";
 import { paginate } from "@/lib/paginate";
 import {
   Pagination as _Pagination,

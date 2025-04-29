fab49dcde2b01fbe3bd5b81f4c1226e49fa27bde
Keiko Iwata <oumelab@pt.rdy.jp>
2025-04-29 22:42:28 +0900

    update: header layout

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import * as React from "react"
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
 
@@ -11,27 +10,31 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Check } from "lucide-react"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="ghost" size="icon" className="cursor-pointer">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem onClick={() => setTheme("light")} className="flex items-center gap-2">
+          {theme === "light" ? <Check size={16} className="size-4" /> : <span className="w-4"></span>}
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem onClick={() => setTheme("dark")} className="flex items-center gap-2">
+          {theme === "dark"  ? <Check size={16} className="size-4" /> : <span className="w-4"></span>}
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem onClick={() => setTheme("system")} className="flex items-center gap-2">
+          {theme === "system"  ? <Check size={16} className="size-4" /> : <span className="w-4"></span>}
           System
         </DropdownMenuItem>
       </DropdownMenuContent>

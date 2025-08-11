0623a4b4b38252faf5b3227b0b9f9c06682b7792
oume-mini <oumelab@pt.rdy.jp>
2025-08-11 23:45:15 +0900

    feat: add favicon, icon, and apple-icon

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@
 
 export default function Home() {
   return (
-    <div className="w-full mx-auto py-2">
-      <p className="">ポートフォリオとWeb開発の学習雑記帳。<br />フロントエンド中心に学んだことをメモしています。</p>
+    <div className="w-full mx-auto py-2 space-y-4">
+      <p>Web開発入門者の学習雑記帳。<br />フロントエンド中心に学んだことをメモしています。</p>
+      <p>学習中のため、誤った情報を記載している可能性があります。<br />お気付きの点があればご指摘いただけると幸いです。</p>
     </div>
   );
 }

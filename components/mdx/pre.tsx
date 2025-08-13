21bc08e0dfedcf5118046d948bcc1c61e2e9dd2e
oume-mini <oumelab@pt.rdy.jp>
2025-08-14 04:38:03 +0900

    style: update copy button styling in code blocks

--- components/mdx/pre.tsx
+++ components/mdx/pre.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { Check, Clipboard } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { Check, Copy } from 'lucide-react';
 import { DetailedHTMLProps, HTMLAttributes, useRef, useState } from 'react';
 import { Button } from '../ui/button';
-import { cn } from '@/lib/utils';
 
 export default function Pre({
   children,
@@ -32,7 +32,7 @@ export default function Pre({
         onClick={handleClickCopy}
         className={cn('group absolute top-4 right-4 size-8 grid place-content-center text-zinc-400 bg-transparent hover:text-white hover:bg-zinc-700 dark:hover:bg-zinc-900 dark:text-muted-foreground dark:hover:text-white focus-within:text-white focus-within:bg-zinc-700 dark:focus-within:bg-zinc-900 rounded-sm cursor-pointer', isCopied && 'text-white bg-zinc-700 dark:bg-zinc-900')}
       >
-        {isCopied ? <Check className="text-white" /> : <Clipboard className="" />}
+        {isCopied ? <Check className="text-white" /> : <Copy className="" />}
       </Button>
       {children}
     </pre>

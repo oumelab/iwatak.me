24a291acaeab0f58bdce31ca1c6b2114bd778503
oume-mini <oumelab@pt.rdy.jp>
2025-08-14 04:34:26 +0900

    refactor: change dark mode toggle to use Radio Group

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,43 +1,85 @@
-"use client"
+"use client";
 
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
-import { useTheme } from "next-themes"
+import {MoonIcon, SunIcon, LaptopIcon} from "@radix-ui/react-icons";
+import {useTheme} from "next-themes";
 
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Check } from "lucide-react"
+import {cn} from "@/lib/utils";
 
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme()
+  const {theme, setTheme} = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="cursor-pointer">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")} className="flex items-center gap-2">
-          {theme === "light" ? <Check size={16} className="size-4" /> : <span className="w-4"></span>}
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")} className="flex items-center gap-2">
-          {theme === "dark"  ? <Check size={16} className="size-4" /> : <span className="w-4"></span>}
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")} className="flex items-center gap-2">
-          {theme === "system"  ? <Check size={16} className="size-4" /> : <span className="w-4"></span>}
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+    <fieldset className="flex items-center gap-1 rounded-full border border-zinc-200 dark:border-zinc-800 p-[2px]">
+      <legend className="sr-only">Theme</legend>
+      <span
+        className={cn(
+          "size-8 grid place-items-center rounded-full cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-800 hover:text-zinc-900 dark:hover:text-zinc-50",
+          theme === "light"
+            ? "bg-zinc-200 dark:bg-zinc-800"
+            : "text-muted-foreground"
+        )}
+      >
+        <input
+          aria-label="light"
+          type="radio"
+          name="theme"
+          id="light"
+          value="light"
+          onChange={(e) => setTheme(e.target.value)}
+          checked={theme === "light"}
+          className="hidden"
+        />
+        <label htmlFor="light">
+          <SunIcon className="size-4 rotate-0 scale-100 transition-all cursor-pointer" />
+          <span className="sr-only">Light</span>
+        </label>
+      </span>
+      <span
+        className={cn(
+          "size-8 grid place-items-center rounded-full cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-800 hover:text-zinc-900 dark:hover:text-zinc-50",
+          theme === "dark"
+            ? "bg-zinc-200 dark:bg-zinc-800"
+            : "text-muted-foreground"
+        )}
+      >
+        <input
+          aria-label="dark"
+          type="radio"
+          name="theme"
+          id="dark"
+          value="dark"
+          onChange={(e) => setTheme(e.target.value)}
+          checked={theme === "dark"}
+          className="hidden"
+        />
+        <label htmlFor="dark">
+          <MoonIcon className="size-4 transition-all dark:rotate-0 cursor-pointer" />
+          <span className="sr-only">Dark</span>
+        </label>
+      </span>
+      <span
+        className={cn(
+          "size-8 grid place-items-center rounded-full cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-800 hover:text-zinc-900 dark:hover:text-zinc-50",
+          theme === "system"
+            ? "bg-zinc-200 dark:bg-zinc-800"
+            : "text-muted-foreground"
+        )}
+      >
+        <input
+          aria-label="system"
+          type="radio"
+          name="theme"
+          id="system"
+          value="system"
+          onChange={(e) => setTheme(e.target.value)}
+          checked={theme === "system"}
+          className="hidden"
+        />
+        <label htmlFor="system">
+          <LaptopIcon className="size-4 rotate-0 scale-100 transition-all cursor-pointer" />
+          <span className="sr-only">System</span>
+        </label>
+      </span>
+    </fieldset>
+  );
 }
